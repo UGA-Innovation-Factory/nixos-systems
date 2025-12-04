ac6167adb24fca6e3ae9fb7551ea594a711bef6b
Hunter <hdh20267@uga.edu>
2025-12-04 16:28:46 -0500

    nevermind on qt wayland

--- users/python.nix
+++ users/python.nix
@@ -3,7 +3,7 @@
   (pkgs.buildFHSEnv {
     name = "pixi";
     runScript = "pixi";
-    targetPkgs = pkgs: with pkgs; [ pixi pyqt6 ];
+    targetPkgs = pkgs: with pkgs; [ pixi ];
   })
   pkgs.uv
 ]
