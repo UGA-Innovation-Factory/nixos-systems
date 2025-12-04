109a1c8d8505c5f6896308a6d5eb93859e4e0808
Hunter <hdh20267@uga.edu>
2025-12-04 12:06:13 -0500

    remove default software for desktop build (temporarily for build)

--- hosts/nix-desktop.nix
+++ hosts/nix-desktop.nix
@@ -1,4 +1,4 @@
-{ config, lib, modulesPath, ... }:
+{ config, pkgs, lib, modulesPath, ... }:
 
 {
   imports =
@@ -10,14 +10,20 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
   boot.kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "udev.log_priority=3"
-      "rd.systemd.show_status=auto"
-    ];
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "udev.log_priority=3"
+    "rd.systemd.show_status=auto"
+  ];
 
   disko.devices.disk.main.content.partitions.swap.size = "16G";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  
+  environment.systemPackages = lib.mkRemove [
+    pkgs.zoom-us
+    pkgs.wpsoffice
+  ];
+
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
